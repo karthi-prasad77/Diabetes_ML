f9dd3b92310fbd7b35110a24f608e501503a71e2
karthi-prasad77 <karthiprasad44@gmail.com>
2023-07-09 23:18:18 +0530

    Experiment with data

---
+++ DataIngestion/data.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+
+
+class Data:
+
+    def __init__(self, dataUrl: str, filePath: str):
+        self.dataUrl = dataUrl
+        self.filePath = filePath
+
+    def Download(self):
+        data = pd.read_csv(self.dataUrl)
+        data.to_csv(self.filePath, index=False)
+        print("DataSet downloaded successfully....")
+
+
+
+DATASET_URL = "https://raw.githubusercontent.com/MainakRepositor/Diabetes-Prediction-System/master/diabetes.csv"
+
+# DataSet file name
+filePath = os.path.join('./Diabetes_Data.csv')
+
+# download and store the dataset
+DiabetesData = Data(DATASET_URL, filePath)
+
+DiabetesData.Download()
