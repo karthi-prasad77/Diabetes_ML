1eb9560bdc059308dada7e36bb7fc987588ee022
karthi-prasad77 <karthiprasad44@gmail.com>
2023-07-09 23:52:12 +0530

    Homepage component

---
+++ src/components/home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+def home():
+    """
+        home : It describes the homepage of 
+                the application
+    """
+    st.title("Diabetes Prediction System")
+
+    # Home page Iamge
+    st.image("")
+
+    # Description to the application
+    st.markdown('''
+        <p style="font-size: 20px;">
+                
+        </p>
+    ''')
