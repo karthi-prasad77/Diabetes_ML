3067bac58e73c25d5fa74868f33e7639168354a4
karthi-prasad77 <karthiprasad44@gmail.com>
2023-07-09 23:33:46 +0530

    Deploy streamlit

---
+++ src/components/about.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+
+# about the author method
+def about():
+    """
+        about : It describes the info about
+                the author
+    """
+    st.balloons()
+    st.title('Contact Us')
+    st.markdown('''### Name : YaswathKumar S''')
+    st.markdown('''Creative person and self motivate to learn new things''')
+    st.image('')
+    st.markdown('''### LinkedIn: [YaswathKumar]()''')
+    st.markdown('''### Github: [YaswathKumar]()''')
