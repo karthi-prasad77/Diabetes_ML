0c413e4ac609aa5ed15e469561b5ddd463d5eb9a
Karthi Prasad S <107131277+karthi-prasad77@users.noreply.github.com>
2023-07-12 03:03:31 +0530

    Update modelPredict.py

--- src/components/utils/modelPredict.py
+++ src/components/utils/modelPredict.py
@@ -2,11 +2,13 @@ import os
 import pickle
 import numpy as np
 from typing import List
+import requests
 
 MODEL_PATH = 'https://github.com/karthi-prasad77/Diabetes_ML/blob/main/Models/bayes.pkl'
+response = requests.get(MODEL_PATH)
 
 # Deserializing the model
 def bayes_predict(data: List, path=MODEL_PATH):
-    model = pickle.load(open(path, "rb"))
+    model = pickle.load(open(response.content, "rb"))
     prediction = model.predict(np.array(data).reshape(1, -1))
     return prediction
