9f3f1b62fdb9f67fb43b23863183e725b1136db0
Karthi Prasad S <107131277+karthi-prasad77@users.noreply.github.com>
2023-07-12 02:57:51 +0530

    Update modelPredict.py

--- src/components/utils/modelPredict.py
+++ src/components/utils/modelPredict.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 from typing import List
 
-MODEL_PATH = os.path.join('../../../Models/bayes.pkl')
+MODEL_PATH = 'https://github.com/karthi-prasad77/Diabetes_ML/blob/main/Models/bayes.pkl'
 
 # Deserializing the model
 def bayes_predict(data: List, path=MODEL_PATH):
