782f4fe03682c7bd1b65dfcbbd187e21c4d1c4b2
Karthi Prasad S <107131277+karthi-prasad77@users.noreply.github.com>
2023-07-12 11:09:34 +0530

    Update modelPredict.py

--- src/components/utils/modelPredict.py
+++ src/components/utils/modelPredict.py
@@ -3,12 +3,13 @@ import pickle
 import requests
 import numpy as np
 from typing import List
+from io import BytesIO
 
 # Deserializing the model
 def bayes_predict(data: List):
     print("Hello", os.getcwd())
     model_file = f'https://drive.google.com/file/d/1mcBBi5X_kszgv2bN3GU6RQdSoHb_CsJ_/view?usp=drive_link'
-    data = requests.get(model_file)
-    model = pickle.loads(data.content)
+    data = BytesIO(requests.get(model_file).content)
+    model = pickle.load(data)
     prediction = model.predict(np.array(data).reshape(1, -1))
     return prediction
