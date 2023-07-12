8bf4a540e55fb7cb1ab37f1e2a9477d4ff2009f0
Karthi Prasad S <107131277+karthi-prasad77@users.noreply.github.com>
2023-07-12 13:57:40 +0530

    Update predict.py

--- src/components/predict.py
+++ src/components/predict.py
@@ -38,7 +38,7 @@ def app(data):
         st.info("Predicted Successfully")
 
         # Prediction sentences
-        if (prediction == 0):
+        if (prediction == 1):
             st.warning("Patient either has high risk of diabetes mellitus")
         else:
             st.warning("Patient free from diabetes")
