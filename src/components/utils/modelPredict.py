044b6150e40acebe7944b5adcfdff10f429f2336
Karthi Prasad S <107131277+karthi-prasad77@users.noreply.github.com>
2023-07-12 10:57:30 +0530

    Update modelPredict.py

--- src/components/utils/modelPredict.py
+++ src/components/utils/modelPredict.py
@@ -7,6 +7,7 @@ from typing import List
 model_file = f'bayes.pkl'
 # Deserializing the model
 def bayes_predict(data: List, path=model_file):
+    print("Hello", os.getcwd())
     with open(path, 'rb') as file:
         model = pickle.load(file)
     prediction = model.predict(np.array(data).reshape(1, -1))
