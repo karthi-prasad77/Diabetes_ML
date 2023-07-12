0f7cac68ecc55ec75624bea36cfd995cfd3ada42
Karthi Prasad S <107131277+karthi-prasad77@users.noreply.github.com>
2023-07-12 11:13:31 +0530

    Update modelPredict.py

--- src/components/utils/modelPredict.py
+++ src/components/utils/modelPredict.py
@@ -8,7 +8,7 @@ from io import BytesIO
 # Deserializing the model
 def bayes_predict(data: List):
     print("Hello", os.getcwd())
-    model_file = f'https://drive.google.com/file/d/1mcBBi5X_kszgv2bN3GU6RQdSoHb_CsJ_/view?usp=drive_link'
+    model_file = f'https://raw.githubusercontent.com/karthi-prasad77/Diabetes_ML/blob/main/Models/bayes.pkl'
     data = BytesIO(requests.get(model_file).content)
     model = pickle.load(data)
     prediction = model.predict(np.array(data).reshape(1, -1))
